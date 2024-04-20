3b2461b265f5cdeeeb2f6c4c6bc3ea794ca98ca6
Karthick K <115405747+karthickk17@users.noreply.github.com>
2024-04-20 15:28:26 +0530

    Update app.py
    Inital version makes it diffcult to process the current query of the user. Now the query can be processed and add to the history easily

--- app.py
+++ app.py
@@ -8,17 +8,22 @@ if "sum_history" not in st.session_state:
 with tab1:
     qa_messages = st.container(height=500, border=False)
     prompt_tab1 = st.chat_input("Say something", key="tab1")
+    for message in st.session_state.qa_history:
+        qa_messages.chat_message(message["role"]).markdown(message["content"])
     if prompt_tab1:
         st.session_state.qa_history.append({"role": "user", "content": prompt_tab1})
         st.session_state.qa_history.append({"role": "assistant", "content": f'Echo: {prompt_tab1}'})
-    for message in st.session_state.qa_history:
-        qa_messages.chat_message(message["role"]).write(message["content"])
+        qa_messages.chat_message("user").markdown(prompt_tab1)
+        qa_messages.chat_message("assistant").markdown(f'Echo: {prompt_tab1}')
+
 
 with tab2:
     sum_messages = st.container(height=500, border=False)
     prompt_tab2 = st.chat_input("Say something", key="tab2")
+    for message in st.session_state.sum_history:
+        sum_messages.chat_message(message["role"]).markdown(message["content"])
     if prompt_tab2:
         st.session_state.sum_history.append({"role": "user", "content": prompt_tab2})
         st.session_state.sum_history.append({"role": "assistant", "content": f'Echo: {prompt_tab2}'})
-    for message in st.session_state.sum_history:
-        sum_messages.chat_message(message["role"]).write(message["content"])
+        sum_messages.chat_message("user").markdown(prompt_tab2)
+        sum_messages.chat_message("assistant").markdown(f'Echo: {prompt_tab2}')
